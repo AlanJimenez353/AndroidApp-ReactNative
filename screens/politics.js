aa503503780d4914becbcf068ab31eb331763922
Alan Jimenez <ajimenez@technisys.com>
2022-01-10 15:14:01 -0300

    +Firebase Autenticator

--- screens/politics.js
+++ screens/politics.js
@@ -1,14 +1,40 @@
-import React from 'react'
-import {View,Text} from 'react-native'
+import { useNavigation } from '@react-navigation/native';
+import React, { useEffect } from 'react'
+import {View,Text, FlatList, ActivityIndicator} from 'react-native'
+import {useDispatch, useSelector} from 'react-redux'
+import { CategoryButton } from '../components/CategoryButton';
+import { HomeStack } from '../navigation/HomeStack';
+import { getCategories, selectCategory } from '../store/actions/categories.actions';
+import { filterComments } from '../store/actions/comments.action';
+
 
 
 export const politics=()=>{
 
-
+    const dispatch = useDispatch();
+    const navigation=useNavigation()
+    const comments=useSelector(state => state.comments)
+    console.log(comments)
     return(
-        <View>
-            <Text>Politics View</Text>
-        </View>
+        <>
+        {filterComments 
+        ? 
+           <View>
+           <FlatList
+           data={filterComments}
+           keyExtractor={category => category.toString()}
+           renderItem={({item}) => {
+               return <CategoryButton title={item.comment}></CategoryButton>
+           }}
+           ></FlatList>
+           </View> 
+
+        :
+        <ActivityIndicator size="large" color='blue'/>
+        
+        }
+        </>
+    
     )
 
 
